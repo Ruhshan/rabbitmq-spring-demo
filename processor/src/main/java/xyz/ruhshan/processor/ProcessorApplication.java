b806186fd04c3338dd3c44ad2b7eef8884d8ddb9
Ruhshan <ruhshan.ahmed@gmail.com>
2022-03-29 18:37:27 +0600

    Add processor module

---
+++ processor/src/main/java/xyz/ruhshan/processor/ProcessorApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruhshan.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessorApplication.class, args);
+    }
+
+}
