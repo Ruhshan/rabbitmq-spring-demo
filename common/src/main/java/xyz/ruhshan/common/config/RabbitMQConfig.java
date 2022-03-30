10bd878a66aafaa6e09b5bf1e238eb45cfd87851
Ruhshan <ruhshan.ahmed@gmail.com>
2022-03-30 11:43:36 +0600

    Enable publishing message as text

--- common/src/main/java/xyz/ruhshan/common/config/RabbitMQConfig.java
+++ common/src/main/java/xyz/ruhshan/common/config/RabbitMQConfig.java
@@ -8,6 +8,8 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,26 @@ public class RabbitMQConfig {
     @Bean
     public Queue createUserRegistrationQueue() {
         return new Queue("q.user-registration");
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public CachingConnectionFactory cachingConnectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory());
+        template.setMessageConverter(converter);
+        return template;
     }
 
 }
