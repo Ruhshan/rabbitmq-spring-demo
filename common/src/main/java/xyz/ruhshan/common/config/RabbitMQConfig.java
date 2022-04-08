290f3e7ef1842dab4fcd71e6c380aa2ea0310b0c
ruhshan-abir <ruhshan.abir@bkash.com>
2022-04-08 15:01:34 +0600

    Enable using default caching connection factory

--- common/src/main/java/xyz/ruhshan/common/config/RabbitMQConfig.java
+++ common/src/main/java/xyz/ruhshan/common/config/RabbitMQConfig.java
@@ -1,13 +1,8 @@
 package xyz.ruhshan.common.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +11,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class RabbitMQConfig {
+    private final CachingConnectionFactory cachingConnectionFactory;
+
+    public RabbitMQConfig(CachingConnectionFactory cachingConnectionFactory) {
+        this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
     @Bean
     public Queue createUserRegistrationQueue() {
         return new Queue("q.user-registration");
@@ -27,16 +28,8 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public CachingConnectionFactory cachingConnectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        return connectionFactory;
-    }
-
-    @Bean
     public RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter converter) {
-        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory());
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
         template.setMessageConverter(converter);
         return template;
     }
